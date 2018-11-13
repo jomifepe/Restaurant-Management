a7076db4d66c6d77c267f5b22ca65fa652912b48
2161789 <2161789@my.ipleiria.pt>
2018-11-13 01:16:48 +0000

    Created a basic index page

---
+++ resources/js/menu.js
@@ -0,0 +1,5 @@
+/*jshint esversion: 6 */
+
+"use strict";
+
+require('./bootstrap');
---
+++ resources/views/vue/index.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+
+@section('title', 'Restaurant Management')
+
+@section('content')
+
+<h1>Hello World</h1>
+
+
+@endsection
+
+@section('pagescript')
+<script src="js/vue.js"></script>
+@stop  
